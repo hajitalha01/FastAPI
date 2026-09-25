2de9091327dc3fc6f78e670ee977597d23ca0580
Haji Talha <mr.haji3360@gmail.com>
2026-09-25 09:43:33 -0700

    Add patient management API

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data = json.load(f)
+
+    return data
+
+
 @app.get("/")
 def home():
-    return {"message": "Hello FastAPI"}
+    return {"message": "Patient Management System API"}
+
 
 @app.get("/about")
 def about():
-    return{"message":"my first api make using fastapi"}
+    return {"message": "A Fully Functional API to Manage Your Patient Records"}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
