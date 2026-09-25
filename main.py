cb85793e1d72b2d2874a4dd6fa4bad5f6cacaa2e
Haji Talha <mr.haji3360@gmail.com>
2026-09-25 05:53:48 -0700

    Initial FastAPI project

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "Hello FastAPI"}
+
+@app.get("/about")
+def about():
+    return{"message":"my first api make using fastapi"}
